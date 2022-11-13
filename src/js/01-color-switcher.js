5676e06a0885282757a40c0d0bb5ef11aa358761
Toma1402 <toma.k1402@gmail.com>
2022-11-13 14:49:23 +0000

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
+
+stopBtnRef.disabled = true;
+startBtnRef.addEventListener('click', onStartClick);
+stopBtnRef.addEventListener('click', onStopClick);
+function onStartClick() {
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+console.dir(startBtnRef);
+function onStopClick() {
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+  clearTimeout(timerId);
+}
